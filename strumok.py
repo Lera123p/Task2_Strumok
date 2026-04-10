c3086ea7adf3b96f6a3a0fed7b5cde686d508734
pepsik123 <mezeviclera@gmail.com>
2026-04-10 15:24:12 +0300

    Editing strumok.py

--- strumok.py
+++ strumok.py
@@ -65,7 +65,32 @@ class Strumok:
             
         self.change_state(is_mixing=False)
 
-
+    def init_512(self, key, IV):
+        self.r1 = 0
+        self.r2 = 0
+        
+        self.s[0]  = key[7] ^ IV[0]
+        self.s[1]  = key[6]
+        self.s[2]  = key[5] ^ IV[1]
+        self.s[3]  = key[4] ^ IV[2]
+        self.s[4]  = key[3]
+        self.s[5]  = key[2] ^ IV[3]
+        self.s[6]  = invert(key[1])
+        self.s[7]  = invert(key[0])
+        
+        self.s[8]  = key[7]
+        self.s[9]  = key[6]
+        self.s[10] = invert(key[5])
+        self.s[11] = key[4]
+        self.s[12] = key[3]
+        self.s[13] = invert(key[2])
+        self.s[14] = key[1]
+        self.s[15] = invert(key[0])
+        
+        for i in range(32):
+            self.change_state(is_mixing=True)
+            
+        self.change_state(is_mixing=False)
 
   def get_number64(self):
         blend_last_cell = fsm_func(self.s[15], self.r1, self.r2)
